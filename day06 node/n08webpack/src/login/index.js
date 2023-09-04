f30bc8f53c8d0e9549c8bd45f34c303172011a64
mailab <z1563492018@gmail.com>
2023-09-04 17:10:44 +0800

    目标9 登录功能实现

--- day06 node/n08webpack/src/login/index.js
+++ day06 node/n08webpack/src/login/index.js
@@ -4,20 +4,20 @@ console.log(checkPhone('13212345678'));
 console.log(checkCode('13212345'));
 
 // 目标2 用户登录判断
-document.querySelector('.btn').addEventListener('click',()=>{
-    const phone = document.querySelector('.login-form [name = mobile]').value
-    const code = document.querySelector('.login-form [name = code]').value
+// document.querySelector('.btn').addEventListener('click',()=>{
+//     const phone = document.querySelector('.login-form [name = mobile]').value
+//     const code = document.querySelector('.login-form [name = code]').value
 
-    if (!checkPhone(phone)) {
-        console.log('手机号长度必须是11位');
-        return
-    }
-    if (!checkCode(code)) {
-        console.log('密码长度必须是6位');
-        return
-    }
-    console.log('提交到服务器');
-})
+//     if (!checkPhone(phone)) {
+//         console.log('手机号长度必须是11位');
+//         return
+//     }
+//     if (!checkCode(code)) {
+//         console.log('密码长度必须是6位');
+//         return
+//     }
+//     console.log('提交到服务器');
+// })
 //  目标3 HtmlWebpackPlugin插件自动打包html
 //  目标4 cssloader自动打包css
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -32,3 +32,43 @@ import imgObj from  './assets/logo.png'
 const theImg = document.createElement('img')
 theImg.src = imgObj
 document.querySelector('.login-wrap').appendChild(theImg)
+
+// 目标9 登录功能
+// 9.1 npm 安装axios
+// 9.2 utils里对应文件导出实现函数
+// 9.3 逻辑代码编写
+// 普通导入
+import myAxios from '../utils/request.js'
+// 命名导入
+import { myAlert } from '../utils/alert.js';
+document.querySelector('.btn').addEventListener('click', () => {
+    const phone = document.querySelector('.login-form [name=mobile]').value
+    const code = document.querySelector('.login-form [name=code]').value
+  
+    if (!checkPhone(phone)) {
+      myAlert(false, '手机号长度必须是11位')
+      console.log('手机号长度必须是11位')
+      return
+    }
+  
+    if (!checkCode(code)) {
+      myAlert(false, '验证码长度必须是6位')
+      console.log('验证码长度必须是6位')
+      return
+    }
+  
+    myAxios({
+      url: '/v1_0/authorizations',
+      method: 'POST',
+      data: {
+        mobile: phone,
+        code: code
+      }
+    }).then(res => { 
+      myAlert(true, '登录成功')
+      
+    }).catch(error => {
+      myAlert(false, error.response.data.message)
+    })
+  })
+
